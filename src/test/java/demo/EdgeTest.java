f2120ab451f5213bdd69d1fdf78af5d07143c9a1
Priyaranjan Das <ranajn53@gmail.com>
2022-04-02 09:00:34 +0530

    changes

--- src/test/java/demo/EdgeTest.java
+++ src/test/java/demo/EdgeTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class ChromeTest {
+public class EdgeTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
